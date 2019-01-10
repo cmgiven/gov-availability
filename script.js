d4ab0d6d8567a45a31a437d0aea5301526c68a09
David Keane <dave.keane@gmail.com>
2019-01-10 13:14:15 +1300

    Rewrite date math for old administrations

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 function update(data) {
-    var thisYear = new Date().getFullYear() + '-01-01T00:00:00-05:00'
+    var thisYear = { start: new Date().getFullYear() + '-01-01T00:00:00-05:00', end : null}
 
     setAvailability(
         'this-year',
-         1 - aggregateTimeRangesAfterDate(data.shutdowns, thisYear) /
-            differenceOfTimeRange({ 'start': thisYear })
+         1 - aggregateTimeRangesWithinRange(data.shutdowns, thisYear) /
+            differenceOfTimeRange(thisYear)
     );
     setAvailability(
         'this-administration',
-         1 - aggregateTimeRangesAfterDate(data.shutdowns, data.trump.start) /
+         1 - aggregateTimeRangesWithinRange(data.shutdowns, data.trump) /
             differenceOfTimeRange(data.trump)
     );
     setAvailability(
         'all-time',
-         1 - aggregateTimeRangesAfterDate(data.shutdowns, data.usa.start) /
+         1 - aggregateTimeRangesWithinRange(data.shutdowns, data.usa) /
             differenceOfTimeRange(data.usa)
     );
     setAvailability(
         'obama-administration',
-         1 - aggregateTimeRangesAfterDate(data.shutdowns, data.obama.start) /
+         1 - aggregateTimeRangesWithinRange(data.shutdowns, data.obama) /
             differenceOfTimeRange(data.obama)
     );
     setAvailability(
         'bushjr-administration',
-         1 - aggregateTimeRangesAfterDate(data.shutdowns, data.bushjr.start) /
+         1 - aggregateTimeRangesWithinRange(data.shutdowns, data.bushjr) /
             differenceOfTimeRange(data.bushjr)
     );
     setAvailability(
         'clinton-administration',
-         1 - aggregateTimeRangesAfterDate(data.shutdowns, data.clinton.start) /
+         1 - aggregateTimeRangesWithinRange(data.shutdowns, data.clinton) /
             differenceOfTimeRange(data.clinton)
     );
     setAvailability(
         'bushsr-administration',
-         1 - aggregateTimeRangesAfterDate(data.shutdowns, data.bushsr.start) /
+         1 - aggregateTimeRangesWithinRange(data.shutdowns, data.bushsr) /
             differenceOfTimeRange(data.bushsr)
     );
     setAvailability(
         'reagan-administration',
-         1 - aggregateTimeRangesAfterDate(data.shutdowns, data.reagan.start) /
+         1 - aggregateTimeRangesWithinRange(data.shutdowns, data.reagan) /
             differenceOfTimeRange(data.reagan)
     );
     setAvailability(
         'carter-administration',
-         1 - aggregateTimeRangesAfterDate(data.shutdowns, data.carter.start) /
+         1 - aggregateTimeRangesWithinRange(data.shutdowns, data.carter) /
             differenceOfTimeRange(data.carter)
     );
 }
@@ -55,32 +55,30 @@ function setAvailability(id, percentage) {
     element.className = percentage < 0.99 ? 'bad' : '';
 }
 
-function aggregateTimeRangesAfterDate(ranges, filterDate) {
+function aggregateTimeRangesWithinRange(ranges, outerRange) {
     var i, range;
     var time = 0;
 
     for (i = ranges.length - 1; i >= 0; i--) {
         range = ranges[i];
 
-        if (range.start > filterDate) {
-            time += differenceOfTimeRange(range);
-        } else if (!range.end || range.end > filterDate) {
-            time += differenceOfTimeRange({
-                'start': filterDate,
-                'end': range.end
-            });
-        } else {
-            break;
-        }
+        time += rangeIntersectTime(range, outerRange)
     }
 
     return time;
 }
 
+function timeOrNow(date){
+    return date == null ? new Date() : new Date(date);
+}
+
+function rangeIntersectTime(range1, range2){
+    return Math.max(0,
+        Math.min(timeOrNow(range1.end).getTime(), timeOrNow(range2.end).getTime())-Math.max(timeOrNow(range1.start).getTime(), timeOrNow(range2.start).getTime()))
+}
+
 function differenceOfTimeRange(range) {
-    var start = new Date(range.start);
-    var end = range.end ? new Date(range.end) : new Date();
-    return end.getTime() - start.getTime();
+    return timeOrNow(range.end).getTime() - timeOrNow(range.start).getTime();
 }
 
 var xhttp = new XMLHttpRequest();
